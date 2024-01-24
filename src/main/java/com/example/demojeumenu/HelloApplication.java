c05875e171cb96b531ea0a614fc303a6cbbe46cf
thibault <thibaultcourcol@gmail.com>
2024-01-24 12:48:19 +0100

    mode fenetrer et affichage du board.png

--- src/main/java/com/example/demojeumenu/HelloApplication.java
+++ src/main/java/com/example/demojeumenu/HelloApplication.java
@@ -3,8 +3,11 @@ package com.example.demojeumenu;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,7 +17,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        double screenWidth = screenBounds.getWidth();
+        double screenHeight = screenBounds.getHeight();
+        Scene scene = new Scene(fxmlLoader.load(), screenWidth, screenHeight);
 
         URL boutonUrl = getClass().getResource("bouton.png");
         URL fondUrl = getClass().getResource("fond.png");
@@ -32,10 +39,19 @@ public class HelloApplication extends Application {
             }
         }
 
-
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
         stage.setTitle("Hello!");
+        stage.setWidth(screenWidth-10);
+        stage.setHeight(screenHeight-50);
         stage.setScene(scene);
+
+        // Personnaliser la fenêtre pour qu'elle occupe tout l'écran
+        stage.setFullScreen(false);
+
+        // activer les boutons de la fenêtre (réduire, agrandir, fermer)
+        stage.initStyle(StageStyle.UNIFIED);
+
+
         stage.show();
     }
 
--- src/main/java/com/example/demojeumenu/HelloController.java
+++ src/main/java/com/example/demojeumenu/HelloController.java
@@ -3,29 +3,24 @@ package com.example.demojeumenu;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
 public class HelloController {
     public Button bouton;
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     protected void onButton1Click() {
-        System.out.println("Button 1 was clicked");
+        System.out.println("Bouton 1 a été cliqué");
     }
 
     @FXML
     protected void onButton2Click() {
-        System.out.println("Button 2 was clicked");
+        System.out.println("Bouton 2 a été cliqué");
     }
 
     @FXML
     protected void onButton3Click() {
-        System.out.println("Button 3 was clicked");
+        System.out.println("Bouton 3 a été cliqué");
+        System.exit(0);
     }
 }
