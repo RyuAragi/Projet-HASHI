130d1167d4330ba372ae5f5af21c55c030e832ae
RyuAragi <victorfouqueray@orange.fr>
2024-01-29 12:05:44 +0100

    Update Jeu.java

--- jeu/Jeu.java
+++ jeu/Jeu.java
@@ -9,6 +9,16 @@ public class Jeu{
 
     }
     boolean verifPont(Noeud n1, Noeud n2){
-        return true;
+        if (n1.x < n2.x){
+            if(n1.ponts.get("E")<= mat_res[n1.x][n1.y].ponts.get("E") ) return true;
+        }if(n1.x > n2.x){
+            if(n1.ponts.get("O")<= mat_res[n1.x][n1.y].ponts.get("O") ) return true;
+        }if(n1.y > n2.y){
+            if(n1.ponts.get("S")<= mat_res[n1.x][n1.y].ponts.get("S") ) return true;
+        }if(n1.y < n2.y){
+            if(n1.ponts.get("N")<= mat_res[n1.x][n1.y].ponts.get("N") ) return true;
+        }
+        mat_err = mat;
+        return false;
     }
 }
