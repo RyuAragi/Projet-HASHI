043bbf31ee96d781919350ee8dff4cb44662ca0a
RyuAragi <victorfouqueray@orange.fr>
2024-01-29 11:28:28 +0100

    premiere version noeud et jeu

---
+++ jeu/Jeu.java
@@ -0,0 +1,14 @@
+public class Jeu{
+    Noeud[][] mat ;
+    Noeud[][] mat_res ;
+    Noeud[][] mat_err ;
+    int taille_li ;
+    int taille_col ;
+
+    Jeu(){
+
+    }
+    boolean verifPont(Noeud n1, Noeud n2){
+        return true;
+    }
+}
---
+++ jeu/Noeud.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+/**Classe noeud utilisé pour symboliser les iles du jeu Hashi, elle est capable de se créer */
+//* @author COUPE Xavier FOUQUERAY Victor*/
+//*@version 0.0 */
+
+
+public class Noeud {
+    /** coordonnée x */
+    int x;
+    /** coordonnée y */
+    int y;
+    /** étiquette qui correspond au nombre de ponts reliés necessaire */
+    int etiquette;
+    /**indique la correspondance du nombre de pont en fonction de la direction (N pour nord, S pour sud, E pour est et O pour ouest) */
+    HashMap<String,Integer> ponts;
+    /**Constructeur @param c_x La coordonnée x du noeud @param c_y la coordonnée y du noeud @param nb_pmax l'étiquette (le nombre de pont a posé) du noeud*/
+    Noeud(int c_x,int c_y , int nb_pmax ){
+        x=c_x;
+        y=c_y;
+        etiquette = nb_pmax;
+        ponts = new HashMap<String,Integer>();
+        ponts.put("N",0);
+        ponts.put("S",0);
+        ponts.put("E",0);
+        ponts.put("O",0);
+    }
+}
