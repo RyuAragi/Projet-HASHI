31d99edd8acdd7cca070e8c54fedd34754264ac7
RyuAragi <victorfouqueray@orange.fr>
2024-01-30 09:11:09 +0100

    Update Jeu.java

--- jeu/Jeu.java
+++ jeu/Jeu.java
@@ -1,13 +1,29 @@
-public class Jeu{
-    Noeud[][] mat ;
-    Noeud[][] mat_res ;
-    Noeud[][] mat_err ;
-    int taille_li ;
-    int taille_col ;
+/** Classe jeu utilisé pour symboliser une grille de hashi*/
+//* @author COUPE Xavier FOUQUERAY Victor*/
+//* @version 0.0 */
 
+
+public class Jeu{
+    /**La matrice sur laquelle nous sommes actuellement entrain de corriger */
+    Noeud[][] mat ;
+    /**La matrice deja finis donc celle que l'on doit avoir au final */
+    Noeud[][] mat_res ;
+    /**La matrice que l'on sauvegardera au moment ou l'on fait une erreur */
+    Noeud[][] mat_err ;
+    /**Le nombre de lignes que la grille possède */
+    int taille_li ;
+    /** Le nombre de colonnes que la grille possède */
+    int taille_col ;
+    /**Constructeur @param o */
     Jeu(){
 
     }
+    /**
+     * Fonction permettant dans un premier temps de detecter dans quel sens se situe le noeud n2 par rapport au n1 et de vérifier si le pont poser est bon ou non
+     * @param n1 le premier noeud
+     * @param n2 le deuxieme noeud
+     * @return true ou false en fonction de si le pont est valide ou pas
+     */
     boolean verifPont(Noeud n1, Noeud n2){
         if (n1.x < n2.x){
             if(n1.ponts.get("E")<= mat_res[n1.x][n1.y].ponts.get("E") ) return true;
