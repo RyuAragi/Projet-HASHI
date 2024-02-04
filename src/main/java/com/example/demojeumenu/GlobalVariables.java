1693ed5cc16c97f39713bf5b780f91d4299b4a9d
thibault <thibaultcourcol@gmail.com>
2024-02-04 21:15:36 +0100

    [update] ajout de getUser() dans GlobalVariables

--- src/main/java/com/example/demojeumenu/GlobalVariables.java
+++ src/main/java/com/example/demojeumenu/GlobalVariables.java
@@ -5,4 +5,10 @@ public class GlobalVariables {
     // Variables globales le nom de l'utilisateur
     public static String userInput;
 
+    // méthode pour récupérer le nom de l'utilisateur
+    public static String getUser() {
+        System.out.println("GlobalVariables.getUser() : " + userInput);
+        return userInput;
+    }
+
 }
