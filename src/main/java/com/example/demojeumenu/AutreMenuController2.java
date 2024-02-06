cdbc8bcc25a09e4e72c01322e3b50ff57ffb7c0e
thibault <thibaultcourcol@gmail.com>
2024-02-07 00:28:13 +0100

    update

--- src/main/java/com/example/demojeumenu/AutreMenuController2.java
+++ src/main/java/com/example/demojeumenu/AutreMenuController2.java
@@ -15,6 +15,7 @@ public class AutreMenuController2 extends BaseController {
 
     @FXML
     private void btn1() {
+        FXMLUtils.loadFXML("AutreMenu3.fxml", scene);
 
     }
 
@@ -23,4 +24,8 @@ public class AutreMenuController2 extends BaseController {
         FXMLUtils.loadFXML("MenuPrincipal.fxml", scene);
     }
 
+    @FXML
+    private void btn2() {
+        FXMLUtils.loadFXML("Parametres.fxml", scene);
+    }
 }
