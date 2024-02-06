322c48ee07323888ac60a8c6119030c60f660c19
Medvil03 <remy@pop-os.localdomain>
2024-02-06 09:23:36 +0100

    Ajout des niveaux faciles et d'une methode pour afficher la matrice

--- jeu/Jeu.java
+++ jeu/Jeu.java
@@ -62,12 +62,6 @@ public class Jeu{
         }catch(IOException e){
             e.printStackTrace();
         }
-
-
-        
-
-
-
     }
     /**
      * Fonction permettant dans un premier temps de detecter dans quel sens se situe le noeud n2 par rapport au n1 et de vérifier si le pont poser est bon ou non
@@ -152,6 +146,7 @@ public class Jeu{
     void commencer(){
         temps_init=System.nanoTime();
     }
+
     /**Méthode fin qui permet de mettre fin au jeu */
     void fin(){
         temps_f=System.nanoTime();
@@ -159,4 +154,20 @@ public class Jeu{
         score=calculScore();
     }
 
+    /**
+     * Methode afficher_mat qui permet d'afficher la matrice des etiquettes
+     */
+    private void afficher_mat() {
+        for(int i = 0; i < this.taille_li; i++) {
+            for(int j = 0; i < this.taille_col; j++) {
+                System.out.print(mat[i][j]);
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Jeu testJeu = new Jeu("./Facile-1.txt");
+        testJeu.afficher_mat();
+    }
 }   
