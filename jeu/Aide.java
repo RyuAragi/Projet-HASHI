9035b211b3c69945066eba3275882fdb0d38b0a4
Xavier Coupe <xavier.coupe.etu@univ-lemans.fr>
2024-02-10 17:38:44 +0100

    Ajout des détections des techniques de démarrage

--- jeu/Aide.java
+++ jeu/Aide.java
@@ -1,28 +1,86 @@
+import java.util.*;
+
 /** Classe aide qui lance la détection de technique et l'aide à la résolution
  * Toutes les méthodes de cette classe sont statiques
 * @author COUPE Xavier FOUQUERAY Victor
 * @version 0.0 
 */
 
+
 public class Aide{
 
 
+    /**
+     * Méthode qui permet de détecter si une technique est applicable
+     * @param grille
+     * @return vrai si une technique est applicable, faux sinon
+     */
     static boolean techniqueDeDepart(Jeu grille){
+        List<Noeud> listeNoeudsGrilleJoueur  = new ArrayList<Noeud>();
+        List<Noeud> listeNoeudsGrilleResolu = new ArrayList<Noeud>();
 
-        return true;
+        int i,j;
+        //int niveauAide;
+
+        for(i = 0; i < grille.getTailleLigne(); i++){
+            for(j = 0; j < grille.getTailleColonne(); j++){
+                if (grille.mat[i][j] != null){
+                    listeNoeudsGrilleJoueur.add(grille.mat[i][j]);
+
+                    /*Pas necessaire de comparer dans les deux matrice ()
+                    * L'emplacement des noeuds est la meme sur les deux matrices
+                    */
+                    listeNoeudsGrilleResolu.add(grille.mat_res[i][j]);
+                }
+            }
+        }
+
+        //niveauAide = grille.getPrecisionAide();
+
+        if (Aide.techniqueHuitAuMilieu(listeNoeudsGrilleJoueur)){
+            return true;
+            
+        }
+        
+        if(Aide.techniqueSixSurLeCote(listeNoeudsGrilleJoueur, listeNoeudsGrilleResolu, grille)){
+            return true;
+            
+        }
+
+        if (Aide.techniqueQuatreSurLeCote(listeNoeudsGrilleJoueur, listeNoeudsGrilleResolu, grille)){
+            return true;
+        }
+        return false;
+        
     }
 
-    static boolean techniqueHuitAuMilieu(Jeu grille){
-        int i,j;
-        for(i = 0; i <grille.getTailleLigne();i++){
-            for(j = 0; j < grille.getTailleColonne();j++){
-                if (grille.mat[i][j] != null && grille.mat[i][j].getEtiquette()==8){
-                        return true;
-                }
+    private static boolean techniqueHuitAuMilieu(List<Noeud> list){
+        for (Noeud n : list){
+            if ((n.getEtiquette() == 8) && (n.getSommeVoisins() < 8)){
+                System.out.println(n.getEtiquette());
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private static boolean techniqueSixSurLeCote(List<Noeud> listJoueur, List<Noeud> listResolu, Jeu grille){
+        
+        for(int i = 0 ; i< listJoueur.size(); i++){
+            if ((listResolu.get(i).getEtiquette() == 6)&&(grille.getNbVoisinReel(listResolu.get(i)) == 3) && (listJoueur.get(i).getSommeVoisins()<6)){
+                return true;
             }
         }
         return false;
     }
 
-
+    private static boolean techniqueQuatreSurLeCote(List<Noeud> listJoueur, List<Noeud> listResolu,Jeu grille){
+        for(int i = 0 ; i< listJoueur.size(); i++){
+            if ((listResolu.get(i).getEtiquette() == 4)&&(grille.getNbVoisinReel(listResolu.get(i)) == 2) && (listJoueur.get(i).getSommeVoisins()<4)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
