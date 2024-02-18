4767bc8a9939ec58db2480f507167ca22ccca701
thibault <thibaultcourcol@gmail.com>
2024-02-18 23:18:40 +0100

    [update] Correction des bugs audio et ajout des sons de clic sur les Boutons
    Signed-off-by: thibault <thibaultcourcol@gmail.com>

--- src/main/java/com/example/demojeumenu/SoundUtils.java
+++ src/main/java/com/example/demojeumenu/SoundUtils.java
@@ -1,33 +1,93 @@
 package com.example.demojeumenu;
 
+import javafx.animation.PauseTransition;
 import javafx.scene.control.Button;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SoundUtils {
 
     private static final AtomicBoolean isPlaying = new AtomicBoolean(false);
-
+    private static MediaPlayer mediaPlayer = null;
     private static int previousSoudLevel = 0;
     private static int soundLevel = 2;
-
+    private static PauseTransition hoverSoundDelay;
 
     public static void addHoverSound(Button button) {
         button.setOnMouseEntered(event -> {
             if (!isPlaying.get()) {
-                new Thread(SoundUtils::playHoverSound).start();
+                hoverSoundDelay = new PauseTransition(Duration.millis(100)); // 200 milliseconds delay
+                hoverSoundDelay.setOnFinished(e -> playHoverSound());
+                hoverSoundDelay.play();
+            }
+        });
+
+        button.setOnMouseExited(event -> {
+            if (hoverSoundDelay != null) {
+                hoverSoundDelay.stop(); // Stop the delay if the mouse leaves the button
+            }
+            if (isPlaying.get()) {
+                stopHoverSound();
             }
         });
     }
 
     private static void playHoverSound() {
-        isPlaying.set(true);
+        if (mediaPlayer != null) {
+            mediaPlayer.stop(); // Stop the previous sound
+        }
         Media sound = new Media(SoundUtils.class.getResource("sounds/hover.mp3").toExternalForm());
-        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer = new MediaPlayer(sound);
         mediaPlayer.setOnEndOfMedia(() -> isPlaying.set(false));
+        isPlaying.set(true);
         mediaPlayer.play();
+    }
+
+    private static void stopHoverSound() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+        isPlaying.set(false);
+    }
+
+    public static void addClickSound(Button button, Runnable additionalAction) {
+        button.setOnAction(event -> {
+            if (!isPlaying.get()) {
+                playClickSound();
+            }
+            additionalAction.run();
+        });
+    }
+
+    private static void playClickSound() {
+        try {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop(); // Stop the previous sound
+            }
+            Media sound = new Media(SoundUtils.class.getResource("sounds/click.mp3").toExternalForm());
+            mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.setOnEndOfMedia(() -> isPlaying.set(false));
+            mediaPlayer.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void playErrorSound() {
+        try {
+            if (mediaPlayer != null) {
+                mediaPlayer.stop(); // Stop the previous sound
+            }
+            Media sound = new Media(SoundUtils.class.getResource("sounds/error.mp3").toExternalForm());
+            mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.setOnEndOfMedia(() -> isPlaying.set(false));
+            mediaPlayer.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     protected static int getSoundLevel(){
