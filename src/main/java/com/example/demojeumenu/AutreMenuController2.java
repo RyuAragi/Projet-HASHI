a073b1ace82fc2680869b4720b95bb459d7fb19f
Tibo_Courcol <44385130+thibcour@users.noreply.github.com>
2024-02-21 13:59:26 +0100

    Thibault (#18)
    * suite dev paramètres
    
    * update
    
    * update
    
    * update
    
    * update
    
    * [update] Ajout de HashMap et ArrayDeque pour la gestion de l'historique des fichiers FXML et d cache des scènes
    
    * [update] Ajout des commentaires dans FXMLUtils.java
    
    * [update]
    
    * [update]
    
    * [update]
    
    * [update]
    
    * [update] ajout hover.mp3, et du cursor
    
    * [update]
    
    * [update]
    
    * [update]
    
    * [update]
    
    * [update]
    
    * [update] cursor + SoundUtils
    
    * Théo (#15)
    
    * suite dev
    
    * suite dev
    
    * suite dev paramètres
    
    * suite dev paramètres
    
    * Delete target/classes directory
    
    ---------
    
    Co-authored-by: Theo.dlrd <Theo.Duluard.Etu@univ-lemans.fr>
    
    * revert
    
    * Delete .idea directory
    
    * [update] cursor + SoundUtils
    
    * [update] cursor + SoundUtils
    
    * [update]
    
    * [update]
    
    * [update]
    
    * [update]
    
    * Delete .idea directory
    
    * [update]
    
    * Delete qodana.yaml
    
    * [update]
    
    * [update] Correction des bugs audio et ajout des sons de clic sur les Boutons
    
    Signed-off-by: thibault <thibaultcourcol@gmail.com>
    
    * [update] Correction des bugs audio et ajout des sons de clic sur les Boutons
    
    Signed-off-by: thibault <thibaultcourcol@gmail.com>
    
    * [update] Correction des bugs audio et ajout des sons de clic sur les Boutons
    
    Signed-off-by: thibault <thibaultcourcol@gmail.com>
    
    * [update] Correction des bugs audio et ajout des sons de clic sur les Boutons
    
    Signed-off-by: thibault <thibaultcourcol@gmail.com>
    
    * update
    
    ---------
    
    Signed-off-by: thibault <thibaultcourcol@gmail.com>
    Co-authored-by: Theo.dlrd <Theo.Duluard.Etu@univ-lemans.fr>

--- src/main/java/com/example/demojeumenu/AutreMenuController2.java
+++ src/main/java/com/example/demojeumenu/AutreMenuController2.java
@@ -1,26 +1,21 @@
 package com.example.demojeumenu;
 
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 
 public class AutreMenuController2 extends BaseController {
 
-    private Scene scene;
-
-    @FXML
-
-    public void setScene(Scene scene) {
-        this.scene = scene;
-    }
-
     @FXML
     private void btn1() {
-
+        FXMLUtils.loadFXML("AutreMenu3.fxml", scene);
     }
 
     @FXML
     private void retour() {
-        FXMLUtils.loadFXML("AutreMenu.fxml", scene);
+        FXMLUtils.goBack(scene);
     }
 
+    @FXML
+    private void btn2() {
+        FXMLUtils.loadFXML("Parametres.fxml", scene);
+    }
 }
