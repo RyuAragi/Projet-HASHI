5dc6fc32fb338959f3df3098d93bedd69aec66be
sedra <a.sedra0201@gmail.com>
2024-03-06 10:26:59 +0100

    Merge remote-tracking branch 'origin/Sedra' into Sedra
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/example/demojeumenu/MenuTailleGrille.java
+++ src/main/java/com/example/demojeumenu/MenuTailleGrille.java
@@ -4,7 +4,15 @@ import javafx.fxml.FXML;
 
 public class MenuTailleGrille extends BaseController{
     @FXML
+    private void btnS() {
+        FXMLUtils.loadFXML("MenuTailleGrille2.fxml", scene);
+    }
+
+    @FXML
     private void retour() {
         FXMLUtils.goBack(scene);
     }
+
 }
+
+
