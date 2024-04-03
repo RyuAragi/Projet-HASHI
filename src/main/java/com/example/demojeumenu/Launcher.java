03331fc04cfd62c20443bc31ea4159359a7f3a45
Medvil03 <rem.georget@gmail.com>
2024-04-03 21:07:58 +0200

    Suppression des fichiers superflus et ajout de la création de l'arborescence

--- src/main/java/com/example/demojeumenu/Launcher.java
+++ src/main/java/com/example/demojeumenu/Launcher.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 
 public class Launcher {
     public static void main(String[] args) throws IOException {
-        Application.launch(App.class, args);
+        //Application.launch(App.class, args);
+        Sauvegarde maSave = new Sauvegarde();
+
+        maSave.creer_arborescence();
+        maSave.creer_dossier_joueur("Nathan");
     }
 }
